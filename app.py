c97a5c304797c762596d81abd92680252db8064d
Ebtesam <ehaque4@gmu.edu>
2021-12-11 22:18:23 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask
-from waitress import serve
+from flask import Flask, request, abort
+from urllib.parse import parse_qs, urlparse
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello():
-    return "Hello World!"
+
+@app.route('/webhook', methods=['POST','GET'])
+def webhook():
+    if request.method == 'GET':
+        print("************************************")
+        print("Response:",request.args['Body'])
+        print("From:",request.args['From'])
+        
+        print("---------------------------------------")
+        return '', 200
+    else:
+        abort(400)
+
+
+@app.route('/mms', methods=['POST','GET'])
+def mms():
+    if request.method == 'POST':
+        print("************************************")
+        # print("Response:",request.args['Body'])
+        # print("From:",request.args['From'])
+        print("Response",request)
+        
+        print("---------------------------------------")
+        return '', 200
+    else:
+        abort(400)
+
 
 if __name__ == '__main__':
     serve(app, host='0.0.0.0', port=8080)
